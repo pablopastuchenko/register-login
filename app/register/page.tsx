0890e8f92fa14ae888c08cc205b258d6d7d158ba
pastuchenko <pablopastuchenkooo@gmail.com>
2024-07-10 14:28:19 -0300

    login/register end

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -18,19 +18,19 @@ export const Register = async() => {
             <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
                 <div className="flex flex-col">
             <Label htmlFor="firstname" className="mb-2">First Name</Label>
-            <Input id="firstname" placeholder="Tyler" type="text" name="firstname" />
+            <Input id="firstname" placeholder="Tyler" type="text" name="firstname" required/>
             </div>
             <div className="flex flex-col">
-            <Label htmlFor="lastname" className="mb-2">Last Name</Label>
-            <Input id="lastname" placeholder="Tyler" type="text" name="lastname" />
+            <Label htmlFor="lastname" className="mb-2" >Last Name</Label>
+            <Input id="lastname" placeholder="Tyler" type="text" name="lastname" required/>
             </div>
             </div>
 
             <Label htmlFor="email">Email Address</Label>
-            <Input id="email" placeholder="project@fc.com" type="email" name="email"/>
+            <Input id="email" placeholder="project@fc.com" type="email" name="email" required/>
             <Label htmlFor="password">Password</Label>
             <Input id="password" placeholder="***********" type="password" className="mb-5"
- name="password"/>
+ name="password" required/>
 
             <button className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]">Sign up &rarr;</button>
 
