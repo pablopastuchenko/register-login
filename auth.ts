92dbe7887033874a3127d0c284495b9d815108df
pastuchenko <pablopastuchenkooo@gmail.com>
2024-07-10 16:34:45 -0300

    github add

--- auth.ts
+++ auth.ts
@@ -1,12 +1,17 @@
-import { compare } from 'bcryptjs';
 import NextAuth, { CredentialsSignin } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import connectDB from "./lib/db";
 import { User } from "./models/User";
+import { compare } from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
+        Github({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        }),
   
         Credentials({
             name: "Credentials",
